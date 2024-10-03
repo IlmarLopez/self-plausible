1214ca81d3dd8e5caf1beae7529d55f6ab870f3b
Ilmar López <ilmarlopezr@hotmail.com>
2024-10-03 16:31:56 -0700

    feat: add infra with cdk

---
+++ cdk/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"os"
+
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/jsii-runtime-go"
+)
+
+func main() {
+	app := awscdk.NewApp(nil)
+
+	NewPlausibleStack(app, "PlausibleStack", &PlausibleStackProps{
+		StackProps: awscdk.StackProps{
+			Env: env(),
+		},
+	})
+
+	app.Synth(nil)
+}
+
+func env() *awscdk.Environment {
+	return &awscdk.Environment{
+		Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
+		Region:  jsii.String(os.Getenv("CDK_DEFAULT_REGION")),
+	}
+}
